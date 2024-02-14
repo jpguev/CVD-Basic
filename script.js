9d03cdf7af0c65b1d2011f8d8c896e3acbcc4f9e
jpguev <aalapaolo@gmail.com>
2024-02-14 15:25:48 +0800

    update90%

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const header = document.querySelector(".header");
+window.console.log(scrollY);
+window.addEventListener("scroll", () => {
+  if (scrollY >= 500) {
+    header.classList.add("open");
+  } else {
+    header.classList.remove("open");
+  }
+});
+
 const burgir = document.querySelector(".burgir__menu");
 const headerNaV = document.querySelector(".header__nav");
 burgir.addEventListener("click", () => {
